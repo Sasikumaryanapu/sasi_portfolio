3d7582bc9c2227e50b992af92fc4df0cc1377c81
sasi kumar <sasikumaryanapur16@gmail.com>
2023-07-20 21:46:21 +0530

    first commit

---
+++ src/data/ProjectData.js
@@ -0,0 +1,89 @@
+import python from "../images/python.png"
+import reactjs from "../images/reactjs.png"
+import mongodb from "../images/mongodb.png"
+import nodejs from "../images/nodejs.png"
+import html from "../images/html.png"
+import css from "../images/css.png"
+import javascript from "../images/javascript.png"
+
+
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    title: "Cityguide",
+    description: "This is an guide about the tourists places for travllers. ",
+    tech_stack: "Reactjs, Css",
+    github_url: "https://github.com/Sasikumaryanapu/citytour.git",
+    demo_url: "https://sasikumaryanapu.github.io/citytour/",
+  },
+  {
+    title: "Instagram Clone",
+    description: "I have created a functional clone of instagram using global state management and single page application library ",
+    tech_stack: "Reactjs, Zustand ,Css",
+    github_url: "https://your_project_github_link",
+    demo_url: "https://your_project_demo_link",
+  },
+  {
+    title: "Rock paper scissor game",
+    description:
+      "A very basic webapp game that is built using DOM manipulations.",
+    tech_stack: "Html, Css, Javascript",
+    github_url: "https://github.com/Sasikumaryanapu/stonepaperscissorgame.git",
+    demo_url: "https://sasikumaryanapu.github.io/stonepaperscissorgame/",
+  },
+  // {
+  //   title: "Portfolio",
+  //   description: "Ofcourse i made my own personal portfolio website.",
+  //   tech_stack: "Reactjs, Css",
+  //   github_url: "https://your_project_github_link",
+  //   demo_url: "https://your_project_demo_link/",
+  // },
+  {
+    title: "RestAPI ",
+    description:"Created an  restapi for notebook using nodejs and mongoDB Atlas database",
+    tech_stack: "Nodejs , Expressjs , MongoDB",
+    github_url: "https://github.com/Sasikumaryanapu/notebook.git",
+    demo_url: "https://github.com/Sasikumaryanapu/notebook.git",
+  },
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: html,
+    name: "HTML",
+  },
+  {
+    img: css,
+    name: "CSS",
+  },
+  {
+    img:javascript,
+    name: "JavaScript",
+  },
+  {
+    img:reactjs,
+    name: "ReactJS",
+  },
+  {
+    img:nodejs,
+    name: "Nodejs",
+  },
+  {
+    img:mongodb,
+    name: "Mongodb",
+  },
+  {
+    img:python,
+    name: "python",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/git.svg",
+    name: "Git",
+  },
+  {
+    img:"https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/responsive.svg",
+    name: "Responsive",
+  }
+];
